d06b4d1b2d8345fdab3d1c5f3e7724a754b832ca
Fabio Benedetti <fabio.benedetti@7-tv.de>
2020-08-02 16:34:44 +0200

    add logos

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import "https://deno.land/x/dotenv/load.ts";
 const token = Deno.env.get("AUTH_TOKEN") as string;
 console.log(token, ' config()["AUTH_TOKEN"]');
 const team = await fetch(
-  "http://api.football-data.org/v2/teams/450/matches?status=FINISHED&limit=2",
+  "http://api.football-data.org/v2/teams/450/matches?status=FINISHED&limit=5",
   {
     headers: {
       "X-Auth-Token": token,
@@ -10,6 +10,34 @@ const team = await fetch(
   }
 ).then((r) => r.json());
 console.log(team);
+
+const promises = team.matches.reverse().map((match: any) => {
+  return fetch(
+    `http://api.football-data.org/v2/teams/${
+      match.homeTeam.id !== 450 ? match.homeTeam.id : match.awayTeam.id
+    }`,
+    {
+      headers: {
+        "X-Auth-Token": token,
+      },
+    }
+  ).then((r) => r.json());
+});
+promises.push(
+  fetch(`http://api.football-data.org/v2/teams/450`, {
+    headers: {
+      "X-Auth-Token": token,
+    },
+  }).then((r) => r.json())
+);
+
+const teamDetails: any[] = await Promise.all(promises);
+
+const displayTeam = (team: { id: string; name: string }) => {
+  const img = teamDetails.find((t) => t.id === team.id).crestUrl;
+  return `<span><img src="${img}" height="15px" />${team.name}</span>`;
+};
+
 let matchesText = ``;
 team.matches.reverse().map((match: any) => {
   const empjy =
@@ -20,7 +48,11 @@ team.matches.reverse().map((match: any) => {
         match.score.fullTime.homeTeam < match.score.fullTime.awayTeam
       ? `😭`
       : `😐`;
-  matchesText += `${match.homeTeam.name} - ${match.awayTeam.name}: ${match.score.fullTime.homeTeam} - ${match.score.fullTime.awayTeam} ${empjy} <br/>`;
+  matchesText += `${displayTeam(match.homeTeam)} ${
+    match.homeTeam.name
+  } - ${displayTeam(match.awayTeam)}: ${match.score.fullTime.homeTeam} - ${
+    match.score.fullTime.awayTeam
+  } ${empjy} <br/>`;
 });
 
 const decoder = new TextDecoder("utf-8");
