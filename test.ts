e7b3852c48d053c743a8d53e138d35e56d54772c
Fabio Benedetti <fabio.benedetti@7-tv.de>
2020-08-02 14:09:19 +0200

    add matches

--- test.ts
+++ test.ts
@@ -1,1 +1,26 @@
-console.log("test");
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+const team = await fetch(
+  "http://api.football-data.org/v2/teams/450/matches?status=FINISHED&limit=2",
+  { headers: { "X-Auth-Token": config()["AUTH_TOKEN"] } }
+).then((r) => r.json());
+console.log(team);
+
+let matchesText = ``;
+const matches = team.matches.map((match: any) => {
+  const empjy =
+    match.homeTeam.id === 450 &&
+    match.score.fullTime.homeTeam > match.score.fullTime.awayTeam
+      ? `😄`
+      : match.homeTeam.id === 450 &&
+        match.score.fullTime.homeTeam < match.score.fullTime.awayTeam
+      ? `😭`
+      : `😐`;
+  matchesText += `${match.homeTeam.name} - ${match.awayTeam.name}: ${match.score.fullTime.homeTeam} - ${match.score.fullTime.awayTeam} ${empjy} \n`;
+});
+
+const decoder = new TextDecoder("utf-8");
+const text = await Deno.readFile("README.tpl.md");
+const decodedText = decoder.decode(text);
+
+console.log(decodedText.replace("%{{teams}}%", `${matchesText}`));
